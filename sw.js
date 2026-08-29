ba08fcdec5ad0ea1eea05cd84fdad1036192385a
josepaolocea-alt <josepaolo.cea@gmail.com>
2026-08-29 20:26:36 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cyn-v41';
+const CACHE = 'cyn-v42';
 const SHELL = ['./index.html', './manifest.json', './premium-dropdown.js?v=31', './premium-datetime.js?v=31'];
 
 self.addEventListener('install', e => {
